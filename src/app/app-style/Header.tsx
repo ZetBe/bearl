4294d598cfe5f705ae00794f62e790f1801294d9
Seo Heewon <seoheewon123@hufs.ac.kr>
2024-02-25 13:20:24 +0900

    fix: title

--- src/app/app-style/Header.tsx
+++ src/app/app-style/Header.tsx
@@ -1,9 +1,20 @@
 import Link from 'next/link'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
 
 export default function Header() {
   return (
     <h1>
-      <Link href="/">bearlr</Link>
+      <Link href="/">bearl</Link>
     </h1>
   )
 }
